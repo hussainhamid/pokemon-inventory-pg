02510bd4caaffc01ee5f42025bfe03a59f9b52e2
hussain <hussain786badgiyar@gmail.com>
2025-04-19 18:12:57 +0530

    just added a little meme

--- controllers/renderFromDbController.js
+++ controllers/renderFromDbController.js
@@ -22,9 +22,10 @@ async function insertPokemonsController(req, res) {
   const secret = req.body.password;
 
   if (secret !== process.env.SECRET_PASSWORD_ENV) {
-    return res
-      .status(401)
-      .send("Unauthorized: Incorrect password. You shall not pass!!");
+    return res.status(401).send(`
+        <h1>Unauthorized: Incorrect password. You shall not pass!!</h1>
+      <div class="tenor-gif-embed" data-postid="16729885" data-share-method="host" data-aspect-ratio="2.40601" data-width="100%"><a href="https://tenor.com/view/you-shall-not-pass-lotr-do-not-enter-not-allowed-scream-gif-16729885">You Shall Not Pass Lotr GIF</a>from <a href="https://tenor.com/search/you+shall+not+pass-gifs">You Shall Not Pass GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+      `);
   }
 
   const pokeIds = [
@@ -58,9 +59,10 @@ async function deleteTeamController(req, res) {
   const secret = req.body.password;
 
   if (secret !== process.env.SECRET_PASSWORD_ENV) {
-    return res
-      .status(401)
-      .send("Unauthorized: Incorrect password. You shall not pass!!");
+    return res.status(401).send(`
+        <h1>Unauthorized: Incorrect password. You shall not pass!!</h1>
+      <div class="tenor-gif-embed" data-postid="16729885" data-share-method="host" data-aspect-ratio="2.40601" data-width="100%"><a href="https://tenor.com/view/you-shall-not-pass-lotr-do-not-enter-not-allowed-scream-gif-16729885">You Shall Not Pass Lotr GIF</a>from <a href="https://tenor.com/search/you+shall+not+pass-gifs">You Shall Not Pass GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+      `);
   }
 
   await db.deleteTeam(trainer);
@@ -80,9 +82,10 @@ async function updateTeamController(req, res) {
   const secret = req.body.password;
 
   if (secret !== process.env.SECRET_PASSWORD_ENV) {
-    return res
-      .status(401)
-      .send("Unauthorized: Incorrect password. You shall not pass!!");
+    return res.status(401).send(`
+        <h1>Unauthorized: Incorrect password. You shall not pass!!</h1>
+      <div class="tenor-gif-embed" data-postid="16729885" data-share-method="host" data-aspect-ratio="2.40601" data-width="100%"><a href="https://tenor.com/view/you-shall-not-pass-lotr-do-not-enter-not-allowed-scream-gif-16729885">You Shall Not Pass Lotr GIF</a>from <a href="https://tenor.com/search/you+shall+not+pass-gifs">You Shall Not Pass GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+      `);
   }
 
   const pokeIds = [
