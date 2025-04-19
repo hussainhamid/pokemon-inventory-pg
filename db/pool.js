76fcbafaed28e8c0aad0814931e7ff7411a12c5b
hussain <hussain786badgiyar@gmail.com>
2025-04-19 19:53:07 +0530

    did some minor changes

--- db/pool.js
+++ db/pool.js
@@ -10,7 +10,7 @@ const { Pool } = require("pg");
 // });
 
 module.exports = new Pool({
-  connectionString: process.env.RENDER_DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
