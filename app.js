e2e8996d439da4af0df089905f31256bc7b9fa66
hussain <hussain786badgiyar@gmail.com>
2025-04-21 16:28:23 +0530

    made port an env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const path = require("node:path");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
